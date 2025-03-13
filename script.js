c71675d0728efe03eb64769d29099e8f2445cce7
kienerxyzzy <kn242158@gmail.com>
2025-03-13 13:10:49 +0700

    NEW SITE!!! YAY!!! VER 5!!!
    NEW SITE!!! YAY!!! VER 5!!! NOW YOU CAN ACTUALLY WRITE AND STUFF! ALSO NEW SUBMIT BUTTON!

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const main = document.getElementById("mainer");
 const title = document.getElementById("title");
 const timer = document.getElementById("timer");
 const tbwritten = document.getElementById("tbwritten");
+const backer = document.getElementById("backer");
+const question = document.getElementById("question");
+var timerInProgress=false;
 function selectIelts() {
   type = "IELTS";
   contest.className = "selbox hidden";
@@ -25,24 +28,55 @@ var cdate = 0;
 var time;
 var second;
 var minute;
-
+var stage=0;
 function selWD() {
   main.className = "selbox mainbox";
   format.className = "selbox hidden";
-  BeginTimer()
+  backer.className = "selector hidden";
+  stage=0;
+  setInterval(UpdateStuffBecauseItTurnsOutYouCantWaitForConditionInJavascipt,100);
+
 }
-function BeginTimer() {
-    cdate = Date.now();
-    setInterval(UpdateTimer,100)
+function UpdateStuffBecauseItTurnsOutYouCantWaitForConditionInJavascipt(){
+  if(stage==0){
+    question.innerHTML="Writing Task 1";
+    BeginTimer(60);
+    stage=1;
+
+  }else if(stage==1){
+    if(!timerInProgress){
+      question.innerHTML="Writing Task 2";
+      stage=2;
+      BeginTimer(120);
+      console.log(tbwritten.value);
+      tbwritten.value=""
+    }
+  }else if(stage==2){
+    if(!timerInProgress){
+      console.log(tbwritten.value);
+      tbwritten.value=""
+      alert("Results are being processed...")
+      stage=3;
+    }
   }
+}
+var id;
+function BeginTimer(time) {
+  timerInProgress=true;
+    cdate = Date.now()+time*1000;
+    id=setInterval(UpdateTimer,100)
+  }
+  
 function UpdateTimer() {
-  time = Math.floor(10 - (Date.now() - cdate) / 1000);
+  time = Math.floor((cdate-Date.now())/1000);
   if(time<=0){
-    timer.innerHTML="TIME IS UP";
+    timer.innerHTML="0:00";
         tbwritten.ariaDisabled=true;
         tbwritten.ariaReadOnly=true;
         tbwritten.disabled=true;
         tbwritten.readOnly=true;
+        clearInterval(id);
+      timerInProgress=false;
     return;
   }else{
     tbwritten.ariaDisabled=false;
@@ -53,4 +87,7 @@ function UpdateTimer() {
   second = time % 60;
   minute = (time - second) / 60;
   timer.innerHTML = minute + ":" + (second >= 10 ? "" : "0") + second;
+}
+function submit(){
+  cdate=0;
 }
