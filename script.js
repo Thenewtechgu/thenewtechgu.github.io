99b7497348dc25316cb7a562b610b0c81f35ed13
kienerxyzzy <kn242158@gmail.com>
2025-04-28 13:32:11 +0700

    the UI overhaul

--- script.js
+++ script.js
@@ -27,8 +27,7 @@ const tbWrite = document.getElementById("tbWrite");
 const backButton = document.getElementById("back");
 const question = document.getElementById("question");
 const ielts = document.getElementById("ielts");
-const fileInput = document.getElementById("file");
-const results = document.getElementById("results-inner");
+const fileInput = document.getElementById("file"); //no longer used, kept to not break code
 const header = document.getElementById("header");
 //const checkbox = document.getElementById("toggle");
 let progressing = false;
@@ -100,7 +99,9 @@ function MakeTOEICQuestion(task, result) {
       Task = [
         `Question ${task}:\nDescribe the following image in one sentence using the following words: ${curr}.`,
         r.split(",")[1],
+        task,
       ];
+      questions = questions.concat([Task]);
     }
   );
 }
@@ -119,6 +120,7 @@ let cdate = 0,
   minute,
   second,
   stage = 0;
+const exam = document.getElementById("exam");
 const selWD = () => {
   HideAll();
   if (type === "IELTS") {
@@ -129,7 +131,68 @@ const selWD = () => {
     stage = 0;
     header.className = "hidden";
     TestForMarked();
-    setInterval(timerProgression, 100);
+    //make toeic questions
+    BeginTimer(60 * 60); //1 hour
+    exam.innerHTML = /*html*/ `
+    <div id="question1"><span class="loader"><br></div>
+    <textarea id="answer1"></textarea><br>
+    <div id="question2"><span class="loader"><br></div>
+    <textarea id="answer2"></textarea><br>
+    <div id="question3"><span class="loader"><br></div>
+    <textarea id="answer3"></textarea><br>
+    <div id="question4"><span class="loader"><br></div>
+    <textarea id="answer4"></textarea><br>
+    <div id="question5"><span class="loader"><br></div>
+    <textarea id="answer5"></textarea><br>
+    <div id="question6"><span class="loader"><br></div>
+    <textarea id="answer6" class="big"></textarea><br>
+    <div id="question7"><span class="loader"><br></div>
+    <textarea id="answer7" class="big"></textarea><br>
+    <div id="question8"><span class="loader"><br></div>
+    <textarea id="answer8" class="big"></textarea><br>
+    `;
+    MakeTOEICQuestion("1", "question1");
+    /*workaround for race conditions */
+    setTimeout(() => {
+      MakeTOEICQuestion("2", "question2");
+    }, 1000);
+
+    setTimeout(() => {
+      MakeTOEICQuestion("3", "question3");
+    }, 1500);
+    setTimeout(() => {
+      MakeTOEICQuestion("4", "question4");
+    }, 2000);
+    setTimeout(() => {
+      MakeTOEICQuestion("5", "question5");
+    }, 2500);
+
+    MakeWT(
+      "TOEIC",
+      "Question 6 (Write a reply to a written request)",
+      "question6",
+      "Question 6:\n",
+      6,
+      "Try to match real tests as closely as possible."
+    );
+
+    MakeWT(
+      "TOEIC",
+      "Question 7 (Write a reply to a written request)",
+      "question7",
+      "Question 7:\n",
+      7,
+      "Try to match real tests as closely as possible."
+    );
+
+    MakeWT(
+      "TOEIC",
+      "Question 8 (Express your opinions in the form of an essay)",
+      "question8",
+      "Question 8:\n",
+      8,
+      "Try to match real tests as closely as possible."
+    );
   }
 };
 const UpdateWD = () => {
@@ -138,8 +201,21 @@ const UpdateWD = () => {
   stage = 0;
   progressing = false;
   header.className = "hidden";
-
-  setInterval(timerProgression, 100);
+  // make IELTS questions
+  exam.innerHTML = "";
+  BeginTimer(60 * 60);
+  if (mode != "2") {
+    exam.innerHTML += /*html*/ `
+    <div id="question1"><span class="loader"><br></div>
+    <textarea id="answer1" class="big"></textarea><br>`;
+    MakeWT("IELTS", "Writing Task 1", "question1", "Writing Task 1:\n", 1);
+  }
+  if (mode != "1") {
+    exam.innerHTML += /*html*/ `
+      <div id="question2"><span class="loader"><br></div>
+      <textarea id="answer2" class="big"></textarea><br>`;
+    MakeWT("IELTS", "Writing Task 2", "question2", "Writing Task 2:\n", 2);
+  }
 };
 function TestForMarked() {
   try {
@@ -153,9 +229,10 @@ function TestForMarked() {
 }
 TestForMarked();
 let generationProgressing = false;
-let Task = "";
+let Task;
 let questions = [];
 let answers = [];
+/*
 const Question = (content = "") => {
   if (!progressing) {
     console.log(tbWrite.value);
@@ -355,6 +432,7 @@ const timerProgression = () => {
 //alert(
 //    'Due to testing purposes, the AI will be temporarily disabled. It is expected to be turned back on by Friday.',
 //);
+*/
 let id;
 const BeginTimer = (time = 9999) => {
   progressing = true;
@@ -383,31 +461,68 @@ const UpdateTimer = () => {
   time = Math.floor((cdate - Date.now()) / 1000);
   if (time <= 0) {
     timer.innerHTML = "0:00";
-    tbWrite.ariaDisabled = true;
-    tbWrite.ariaReadOnly = true;
-    tbWrite.disabled = true;
-    tbWrite.readOnly = true;
     clearInterval(id);
     progressing = false;
     return;
-  } else {
-    tbWrite.ariaDisabled = false;
-    tbWrite.ariaReadOnly = false;
-    tbWrite.disabled = false;
-    tbWrite.readOnly = false;
   }
   second = time % 60;
   minute = (time - second) / 60;
-  timer.innerHTML = `${minute.toString().padStart(2, "0")}:${second.toString().padStart(2, "0")}`;
+  timer.innerHTML = `${minute.toString().padStart(2, "0")}:${second
+    .toString()
+    .padStart(2, "0")}`;
 };
 const submit = () => {
-  if(type!="TOEIC"){
-    cdate = 0;
-    progressing = false;
-  }else{
-    progressing=false;
+  if (type == "TOEIC") {
+    questions.forEach((element) => {
+      if (element.length == 3) {
+        getAIResponseWithImage(
+          `I'm practicing for TOEIC, can you review my answers?\nQuestion:${
+            element[0]
+          }\nMy answer:${
+            document.getElementById("answer" + element[2]).value
+          }\nNote: one thing that is required is the overall Level from 1-9 along with a Scale Score from 0-200. Only when you have provided that, you could explain your scoring.`,
+          element[1]
+        ).then((r) => {
+          document.getElementById("toeic" + element[2]).innerHTML =
+            parseAIOutput(r);
+        });
+      } else {
+        getAIResponse(
+          `I'm practicing for TOEIC, can you review my answers?\nQuestion:${
+            element[0]
+          }\nMy answer:${
+            document.getElementById("answer" + element[1]).value
+          }\nOne thing that is required is the overall Level from 1-9 along with a Scale Score from 0-200. Only when you have provided that, you could explain your scoring.`
+        ).then((r) => {
+          document.getElementById("toeic" + element[1]).innerHTML =
+            parseAIOutput(r);
+        });
+      }
+    });
+    HideAll();
+    document.getElementById("results-outer-toeic").className = "selbox mainbox";
+  } else {
+    questions.forEach((element) => {
+      document.getElementById(
+        "ielts" + element[1]
+      ).innerHTML = /*html*/ `<span class="loader"></span>`;
+      getAIResponse(
+        `I'm practicing for IELTS, can you review my answers?\nQuestion:${
+          element[0]
+        }\nMy answer:${
+          document.getElementById("answer" + element[1]).value
+        }\nA requirement that I need to have is a table with 4 criteria of an IELTS Writing Task: TA, CC, LR, Grammar, each with a band from 1-9, and the overall band. Only when it has already been provided, explain your bands.`
+      ).then((r) => {
+        document.getElementById(
+          "ielts" + element[1]
+        ).innerHTML = /*html*/ `<h3>Writing Task ${
+          element[1]
+        }</h3>${parseAIOutput(r)}`;
+      });
+    });
+    HideAll();
+    document.getElementById("results-outer").className = "selbox mainbox";
   }
-  
 };
 
 const parseAIOutput = (s) => {
@@ -420,10 +535,7 @@ const parseAIOutput = (s) => {
 };
 
 async function getAIResponse(prompt = "") {
-  /*
-  return Promise.resolve(
-    "im sorry but ai has been disabled. you asked " + prompt
-  );*/
+  return Promise.resolve("AI has been disabled. you asked " + prompt);
   const apiKey = "AIzaSyACUiew2xvOhoLEQXiUtcqld7xl0BG4YwY"; // Replace with your actual API key
   const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
@@ -448,9 +560,7 @@ async function getAIResponse(prompt = "") {
   return data.candidates[0].content.parts[0].text;
 }
 async function getAIResponseWithImage(prompt = "", picture = "") {
-  /*return Promise.resolve(
-    "im sorry but ai has been disabled. you asked " + prompt
-  );*/
+  return Promise.resolve("AI has been disabled. you asked " + prompt);
   const apiKey = "AIzaSyACUiew2xvOhoLEQXiUtcqld7xl0BG4YwY"; // Replace with your actual API key
   const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
@@ -502,18 +612,21 @@ const MakeWT = (
   wanted = "foo",
   id = "",
   prefix = "",
-  time = 9999,
+  windex = 0,
   suffix = "Notes: Please don't use photo diagrams - I heard AI's like you have a hard time drawing them. Tables are OK though."
 ) => {
-  cdate = 0;
-  timer.innerHTML = /*html*/`<span class='loader'></span>`;
+  //cdate = 0;
+  //timer.innerHTML = /*html*/ `<span class='loader'></span>`;
   getAIResponse(
     `I'm practicing for ${contest}, can you generate a ${wanted} question for me? I don't want any tips/directions, as I'd like this to be a sort of mock test.\n ${suffix}`
   ).then((r) => {
     generationProgressing = false;
-    Task = cleanGeneratedText(r);
-    document.getElementById(id).innerHTML = parseAIOutput(prefix + Task);
-    BeginTimer(time);
+    Task = [cleanGeneratedText(r), windex];
+    questions = questions.concat([Task]);
+    document.getElementById(id).innerHTML = parseAIOutput(
+      prefix + cleanGeneratedText(r)
+    );
+    //BeginTimer(time);
   });
 };
 console.log(":)");
